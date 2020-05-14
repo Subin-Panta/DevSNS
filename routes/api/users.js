4b631b4f7241f8bdbb85b4411c586da8f5c4d49f
Subin-Panta <subinpanta9@gmail.com>
2020-05-14 22:48:52 +0545

    added jwt token and returned token for user registration need to get token back when the same user logsin

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const gravatar = require('gravatar')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const config = require('config')
 const { check, validationResult } = require('express-validator')
 //importing schema
 const User = require('../../models/User')
@@ -29,7 +31,9 @@ router.post(
       //See if user exists
       let user = await User.findOne({ email })
       if (user) {
-         return res.status(400).json({ errors: [{ msg: 'User Already Exists' }] })
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User Already Exists' }] })
       }
       //Get users gravatar
       const avatar = gravatar.url(email, {
@@ -48,8 +52,21 @@ router.post(
       user.password = await bcrypt.hash(password, salt)
       await user.save()
       //return jsonwebtoken
-
-      res.send('user registered')
+      const payload = {
+        user: {
+          id: user.id
+        }
+      }
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err
+          res.json({ token })
+        }
+      )
+      console.log(payload)
     } catch (err) {
       console.error(err.message)
       res.status(500).send('Server Error')
