0f47fead0026607e1d7a284f95dce5eca3269f25
Subin-Panta <subinpanta9@gmail.com>
2020-07-25 13:06:27 +0545

    deploy bug

---
+++ client/build/precache-manifest.e6ff44ec6e904858078c483337ac313d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a00f445be6ba991dd6fc390b865ceca5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31ecc15534c635fb30ba",
+    "url": "/static/css/main.aa0903e2.chunk.css"
+  },
+  {
+    "revision": "ee9866c705c529d30aef",
+    "url": "/static/js/2.b5e7b8e2.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/static/js/2.b5e7b8e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31ecc15534c635fb30ba",
+    "url": "/static/js/main.274226b7.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
