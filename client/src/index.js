5f8c1bd9ad603aad991a26af07aa0aff4998171e
Subin-Panta <subinpanta9@gmail.com>
2021-02-25 15:43:43 +0545

    build ready

--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,12 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<Router basename='/server'>
+			<App />
+		</Router>
+	</React.StrictMode>,
+	document.getElementById('root')
 )
 
 // If you want your app to work offline and load faster, you can change
